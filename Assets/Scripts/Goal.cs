d93a9262c69551a4fc2b2494cc8831b93eba3e5f
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-10-02 16:09:13 -0400

    Goal Created

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // A static field accessible by code anywhere
+    static public bool        goalMet = false;
+    void OnTriggerEnter( Collider other ) {
+        // When the trigger is hit by something
+        // Check to see if it's a Player
+        if ( other.gameObject.tag == "Player" ) {
+            // If so, set goalMet to true
+            Goal.goalMet = true;
+            // Also set the alpha of the color to higher opacity
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
